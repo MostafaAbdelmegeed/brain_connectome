746728bae66b1ed2930eb4fa7a1820fd715f5115
Mostafa Abdelmegeed <mostafa0as3d@gmail.com>
2024-05-24 14:41:17 -0400

    [ADD] PPMI - AAL116 Asymmetry Analysis

---
+++ graphIO/graphIO.py
@@ -0,0 +1,118 @@
+import numpy as np
+import os
+import json
+import pandas as pd
+from tqdm import tqdm
+import torch
+
+
+def write_adj_matrix(adj_matrix_file_path, adj_matrix):
+    """
+    Writes an adjacency matrix to a file.
+
+    :param adj_matrix_file_path: Path to the file where the adjacency matrix will be written.
+    :param adj_matrix: The adjacency matrix to write to the file.
+    """
+    np.savetxt(adj_matrix_file_path, adj_matrix, fmt='%f')
+
+def read_adj_matrix_from_file(file_path, as_tensor=False):
+    """
+    Reads the adjacency matrix from a single file.
+
+    :param file_path: Path to the file containing the adjacency matrix.
+    :param as_tensor: Boolean flag to return the matrix as a tensor if True.
+    :return: The adjacency matrix as a NumPy array or tensor.
+    """
+    adj_matrix = np.nan_to_num(np.loadtxt(file_path))
+    if as_tensor:
+        adj_matrix = torch.tensor(adj_matrix)
+    return adj_matrix
+
+def read_adj_matrices_from_directory(directory, include_string='', as_tensor=False):
+    """
+    Reads adjacency matrices from files in a directory and stores them in a dictionary.
+    The processing of each file is shown with a progress bar.
+
+    :param directory: Path to the directory containing the files.
+    :param include_string: String to filter the files to include.
+    :param as_tensor: Boolean flag to return matrices as tensors if True.
+    :return: A dictionary where the key is the file name and the value is the adjacency matrix.
+    """
+    adj_matrices = {}  # Dictionary to store adjacency matrices
+    # Get a list of files in the directory
+    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    # Iterate over files in the directory with a progress bar
+    for filename in tqdm(files, desc="Reading adjacency matrices"):
+        filepath = os.path.join(directory, filename)
+        if include_string and include_string in filename:
+            # Read the adjacency matrix from the file
+            adj_matrix = read_adj_matrix_from_file(filepath, as_tensor=as_tensor)
+            # Store the adjacency matrix in the dictionary
+            adj_matrices[os.path.splitext(filename)[0]] = adj_matrix
+    return adj_matrices
+
+def parse_class_name(directory_name):
+    """
+    Parses the class name from the directory name by extracting characters until the first number.
+    
+    :param directory_name: The name of the directory.
+    :return: The class name.
+    """
+    class_name = ''
+    for char in directory_name:
+        if char.isdigit():
+            break
+        class_name += char
+    return class_name
+
+def parse_record_name(file_name):
+    """
+    Parses the record name from the file name by extracting characters before the first underscore.
+    
+    :param file_name: The name of the file.
+    :return: The record name.
+    """
+    return file_name.split('_')[0]
+
+
+def read_ppmi_data(ppmi_directory, method='new', as_tensor=False):
+    """
+    Reads adjacency matrices from files in a directory structure and organizes them in a hierarchical dictionary.
+    
+    :param parent_directory: Path to the parent directory containing the subdirectories.
+    :param as_tensor: Boolean flag to return matrices as tensors if True.
+    :return: A dictionary with class names, record names, and their corresponding adjacency matrices.
+    """
+    hierarchical_dict = {}
+    
+    # Get a list of subdirectories in the parent directory
+    subdirectories = [d for d in os.listdir(ppmi_directory) if os.path.isdir(os.path.join(ppmi_directory, d))]
+    
+    # Iterate over each subdirectory
+    for subdirectory in tqdm(subdirectories, desc="Processing directories"):
+        class_name = parse_class_name(subdirectory)
+        subdirectory_path = os.path.join(ppmi_directory, subdirectory)
+        
+        # Get a list of files in the subdirectory
+        files = [f for f in os.listdir(subdirectory_path) if os.path.isfile(os.path.join(subdirectory_path, f))]
+        
+        for file_name in files:
+            if f'_{method}_' in file_name:
+                record_name = parse_record_name(file_name)
+                file_path = os.path.join(subdirectory_path, file_name)
+                
+                # Read the adjacency matrix from the file
+                adj_matrix = read_adj_matrix_from_file(file_path, as_tensor=as_tensor)
+                
+                # Initialize the class dictionary if it does not exist
+                if class_name not in hierarchical_dict:
+                    hierarchical_dict[class_name] = {}
+                
+                # Add the record and its adjacency matrix to the class dictionary
+                hierarchical_dict[class_name][record_name] = adj_matrix
+    
+    return hierarchical_dict
+
+
+
+
