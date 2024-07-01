6e6c49a5643a87d4b592db0fa3f372522da31d08
Mostafa Abdelmegeed <mostafa0as3d@gmail.com>
2024-07-01 13:42:15 -0400

    [MOD] Now only upper triangle is used for eFCs

--- pipelines/convert_adni_to_eFC_6670_pth.py
+++ pipelines/convert_adni_to_eFC_6670_pth.py
@@ -9,7 +9,7 @@ project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
 from graphIO.io import read_adni_timeseries
 
-# python pipelines/convert_adni_to_eFC_pth.py --source "C:/Users/mosta/OneDrive - UNCG/Academics/CSC 699 - Thesis/data/ADNI" --destination "./data/adni_efc.pth" --gpu 0
+# python pipelines/convert_adni_to_eFC_pth.py --source "C:/Users/mosta/OneDrive - UNCG/Academics/CSC 699 - Thesis/data/ADNI" --destination "./data/adni_efc.pth"
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -17,7 +17,6 @@ def parse_args():
     parser.add_argument('--atlas', type=str, default='AAL116', help='Atlas to use.')
     parser.add_argument('--method', type=str, default='timeseries', help='Use correlation matrices', choices=['correlation', 'curvature', 'timeseries'])
     parser.add_argument('--destination', type=str, default='adni_efc.pth', help='Save path for the processed data.')
-    parser.add_argument('--gpu', type=int, default=0, help='GPU device ID to use.')
     args = parser.parse_args()
     return args
 
@@ -28,31 +27,31 @@ def compute_eTS(timeseries):
     - timeseries: Tensor of shape (N, T), where N is the number of regions and T is the number of time points.
     
     Returns:
-    - eTS: Tensor of shape (N, N, T), representing the edge time series.
+    - eTS: Tensor of shape (N * (N - 1) // 2, T), representing the edge time series.
     """
     N, T = timeseries.shape
     z_timeseries = (timeseries - timeseries.mean(dim=1, keepdim=True)) / timeseries.std(dim=1, keepdim=True)
     eTS = torch.einsum('it,jt->ijt', z_timeseries, z_timeseries)
+    triu_indices = torch.triu_indices(N, N, offset=1)
+    eTS = eTS[triu_indices[0], triu_indices[1]]
     return eTS
 
 def compute_edge_features(eTS):
     """
     Compute edge features from edge time series.
     Args:
-    - eTS: Tensor of shape (N, N, T), representing the edge time series.
+    - eTS: Tensor of shape (N * (N - 1) // 2, T), representing the edge time series.
     
     Returns:
-    - edge_features: Tensor of shape (E, T), where E = N * N, representing the edge features.
+    - edge_features: Tensor of shape (E, T), where E = N * (N - 1) // 2, representing the edge features.
     """
-    N, _, T = eTS.shape
-    edge_features = eTS.reshape(N * N, T)  # Shape: (6670, T)
-    return edge_features
+    return eTS
 
 def compute_eFC(edge_features):
     """
     Compute edge functional connectivity (eFC) from edge features.
     Args:
-    - edge_features: Tensor of shape (E, T), where E = N * N, representing the edge features.
+    - edge_features: Tensor of shape (E, T), where E = N * (N - 1) // 2, representing the edge features.
     
     Returns:
     - eFC: Tensor of shape (E, E), representing the edge functional connectivity.
@@ -70,8 +69,8 @@ def pipeline(timeseries_data):
     - eFC_matrices: Tensor of shape (M, E, E), representing the edge functional connectivity matrices for each record.
     """
     M, N, T = timeseries_data.shape
-    E = N * N  # Number of edges
-    eFC_matrices = torch.zeros((M, E, E), device=timeseries_data.device)
+    E = N * (N - 1) // 2  # Number of edges for an undirected graph
+    eFC_matrices = torch.zeros((M, E, E))
     
     for i in tqdm(range(M), desc="Computing eFC matrices"):
         eTS = compute_eTS(timeseries_data[i])
@@ -87,10 +86,6 @@ if __name__ == "__main__":
     destination = args.destination
     method = args.method
     atlas = args.atlas
-    gpu_id = args.gpu
-
-    # Set the device based on the GPU ID
-    device = torch.device('cpu')
 
     data = read_adni_timeseries(source)
     labels = data['label']
@@ -98,10 +93,6 @@ if __name__ == "__main__":
     
     # Ensure the NumPy array is writable and convert to PyTorch tensor
     timeseries_data = torch.tensor(np.copy(timeseries_data), dtype=torch.float32)
-    
-    # Move data to the selected GPU if available
-    timeseries_data = timeseries_data.to(device)
-    labels = labels.to(device)
     
     print(f'Timeseries data shape: {timeseries_data.shape}, Labels shape: {labels.shape}')
     eFC_matrices = pipeline(timeseries_data)
